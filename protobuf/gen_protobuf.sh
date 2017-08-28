1c97835ff6ade561b490297738b6cd9e36339cd9
aliang <aliang2344@gmail.com>
2017-08-27 21:51:38 -0400

    adding dirname to protobuf gen script

--- protobuf/gen_protobuf.sh
+++ protobuf/gen_protobuf.sh
@@ -1,4 +1,5 @@
 # my GOPATH is more than one dir... otherwise
 # set PATH=$PATH:$GOPATH/bin
+DIR=`dirname $0`
 set PATH=$PATH:~/go/bin
-protoc --go_out=../messages *.proto
+protoc --go_out=messages $DIR/*.proto
